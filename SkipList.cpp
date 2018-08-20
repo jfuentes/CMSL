2ca7cfefa3f3d9c5ef6ce0d934d70491a7b09fcf
Joel Fuentes <jfuentes@ubiobio.cl>
2018-08-20 12:15:57 -0700

    Header info removed

--- SkipList.cpp
+++ SkipList.cpp
@@ -1,15 +1,3 @@
-/**
-***
-*** Copyright  (C) 1985-2011 Intel Corporation. All rights reserved.
-***
-*** The information and source code contained herein is the exclusive
-*** property of Intel Corporation. and may not be disclosed, examined
-*** or reproduced in whole or in part without explicit written authorization
-*** from the company.
-***
-*** ----------------------------------------------------------------------------
-**/
-
 
 #include "cm_rt.h"
 #include "../../compiler/include/cm/cm_vm.h"
